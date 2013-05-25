0f899799ad01fbe90707ade36127da8b8a4c2349
Sudharshan S <sudharsh@gmail.com>
2013-05-26 03:14:51 +0530

    Friendly messages once the minecraft server has been setup

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,8 +2,9 @@ echo "Starting minecraft server" 1>&2
 nohup java -Xms${ms} -Xmx${mx} -jar minecraft_server.jar nogui >> nohup.out &
 echo $! > server.pid
 
-(pgrep -F server.pid && echo "Started server :). Watch out for the creepers") || echo "Couldn't start the minecraft server. Use 'logs'"
+pgrep -F server.pid || (echo "Couldn't start the minecraft server. Use 'logs'" && exit 1)
 
+ubip=$(curl http://ip4.me 2>/dev/null | sed -e 's#<[^>]*>##g' | grep '^[0-9]')
 
-
-
+echo "Started minecraft server successfully! Connect to '$ubip' for Multiplayer"
+echo "Happy mining!"
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,3 @@
 echo "Stopping minecraft server"
 pkill -f minecraft_server.jar
-pgrep minecraft_server || echo "Stopped server"
-
+pgrep minecraft_server || (echo "Server already stopped"; exit 1)
