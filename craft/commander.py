0ee5a31c2295df313e6b185048439f7918ce4109
Sudharshan S <sudharsh@gmail.com>
2013-05-26 03:14:22 +0530

    If setup is invoked, run 'bootstrap', 'fetch' and 'start'.
    Essentially the entire minecraft server can now be up and running with just two command. spawn and setup

--- craft/commander.py
+++ craft/commander.py
@@ -13,15 +13,19 @@ def bootstrap_server(ami_id):
     run("mkdir -p %s" % REMOTE_PATH)
     cd(REMOTE_PATH)
     print(green("Bootstrapping %s" % ami_id))
+    print(green("------"))
     try:
         put(BOOTSTRAP_PATH + ami_id + ".sh", REMOTE_PATH + "bootstrap.sh")
     except ValueError:
         print(red("Couldn't find bootstrap script for AMI:%s" % ami_d))
     run("sh {0}/bootstrap.sh".format(REMOTE_PATH), pty=False, combine_stderr=False)
+    print(green("------"))
+
 
 
 def run_remote(command, command_args=[], remote_vars={}):
     try:
+        print(green("-------"))
         run("mkdir -p %s" % REMOTE_PATH)
         script_file = "{0}/{1}.sh".format(SCRIPTS_PATH, command)
         remote_path = "{0}/{1}.sh".format(REMOTE_PATH, command)
@@ -31,8 +35,14 @@ def run_remote(command, command_args=[], remote_vars={}):
         with open(script_file, "r") as fh:
             prepared_buffer.write(fh.read())
         put(prepared_buffer, remote_path)
+        with cd(REMOTE_PATH):
+            return run("sh ./{0}.sh {1}".format(command, command_args), pty=False, combine_stderr=False)
+        print(green("-------"))
+
     except ValueError, IOError:
         print(traceback.format_exc())
+        print(red("Is the machine up?"))
         return
-    with cd(REMOTE_PATH):
-        run("sh ./{0}.sh {1}".format(command, command_args), pty=False, combine_stderr=False)
+    except:
+        print(red("Error when executing command {0}".format(command)))
+        return
