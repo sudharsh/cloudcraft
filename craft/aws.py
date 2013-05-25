1cdb4b6b4f1420c1d526a604599221533c5e0e2b
Sudharshan S <sudharsh@gmail.com>
2013-05-25 14:54:29 +0530

    Let provision take in kwargs instead of passing the conf object directly

--- craft/aws.py
+++ craft/aws.py
@@ -1,6 +1,18 @@
-def provision(conf):
-    print dir(conf), conf.sections()
-    print "provisioning machines"
+import boto.ec2 as ec2
+
+def __get_connection(token, secret, region):
+    conn = ec2.connect_to_region(region, aws_access_key_id=token,
+                                 aws_secret_access_key=secret)
+    return conn
+
+
+def provision(aws_access_token="", aws_access_secret="",
+              ec2_region="us-west-2", ami="ami-bf1d8a8f",
+              instance_type="m1.small"):
+    instance = {}
+    print "Provisioning instances"
+    conn = __get_connection(aws_access_token, aws_access_secret,
+                            ec2_region)
     return True
 
     
