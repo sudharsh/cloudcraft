9a3c63926c215a3c1f60ddda9921a1f64bffd934
Sudharshan S <sudharsh@gmail.com>
2013-05-26 21:30:01 +0530

    install headless jre

--- bootstrap/ami-bf1d8a8f.sh
+++ bootstrap/ami-bf1d8a8f.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 # WARNING: This will be executed in the remote machine
 
-echo "Installing Java" 1>&2
+echo "Installing Java"
 sudo apt-get update
-sudo apt-get install -y openjdk-7-jdk
-echo "Done installing Java version 7" 1>&2
+sudo apt-get install -y openjdk-7-jre-headless
+echo "Done installing Java version 7"
+
+echo "Creating directory for minecraft"
+mkdir -p ~/minecraft
 
