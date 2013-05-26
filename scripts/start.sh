7995d2a39ea0da5a3cb48a1664fc05f14e4a673f
Sudharshan S <sudharsh@gmail.com>
2013-05-26 15:04:24 +0530

    Gracefully shutdown the server

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,11 @@
-echo "Starting minecraft server" 1>&2
-nohup java -Xms${ms} -Xmx${mx} -jar minecraft_server.jar nogui >> nohup.out &
-echo $! > server.pid
-
-pgrep -F server.pid || (echo "Couldn't start the minecraft server. Use 'logs'" && exit 1)
-
-ubip=$(curl http://ip4.me 2>/dev/null | sed -e 's#<[^>]*>##g' | grep '^[0-9]')
-
-echo "Started minecraft server successfully! Connect to '$ubip' for Multiplayer"
-echo "Happy mining!"
+if pgrep -f "minecraft_server" > /dev/null
+then
+    echo "Minecraft server already running"
+else
+    echo "Starting minecraft server"
+    screen -dmS minecraft java -Xms${ms} -Xmx${mx} -jar minecraft_server.jar nogui
+    pgrep -f "minecraft_server" > /dev/null || (echo "Couldn't start the minecraft server. Use 'logs'" && exit 1)
+    ubip=$(curl http://ip4.me 2>/dev/null | sed -e 's#<[^>]*>##g' | grep '^[0-9]')
+    echo "Started minecraft server successfully! Connect to '$ubip' for Multiplayer"
+    echo "Happy mining!"
+fi
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,12 @@
-echo "Stopping minecraft server"
-pkill -f minecraft_server.jar
-pgrep minecraft_server || (echo "Server already stopped"; exit 1)
+if pgrep -f "minecraft_server" > /dev/null
+then
+    echo "Warning miners..."
+    screen -p 0 -S minecraft -X eval 'stuff "say WARNING: Server shutting down in 10 seconds"\015'
+    echo "Saving map..."
+    screen -p 0 -S minecraft -X eval 'stuff "save-all"\015'
+    sleep 10
+    echo "Stopping server..."
+    screen -p 0 -S minecraft -X eval 'stuff "stop"\015'
+else
+    echo "Server already stopped"
+fi
