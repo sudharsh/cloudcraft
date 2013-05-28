f1474a2e9777cfe6d22d648faac786d5417f2469
Sudharshan S <sudharsh@gmail.com>
2013-05-28 20:11:16 +0530

    Refactor aws.py into an 'api' class. Implement the changes in cloudcraft as well

--- craft/__init__.py
+++ craft/__init__.py
@@ -1,4 +1,5 @@
 import aws
+import mcserver
 
 
     
--- craft/aws.py
+++ craft/aws.py
@@ -8,17 +8,119 @@ from itertools import chain
 import boto.ec2 as ec2
 import boto.exception
 
+import mcserver
 import commander
 
 log = logging.getLogger("cloudcraft")
 logging.getLogger('boto').setLevel(logging.CRITICAL)
 
-def __get_connection(token, secret, region):
-    conn = ec2.connect_to_region(region, aws_access_key_id=token,
-                                 aws_secret_access_key=secret)
-    return conn
+
+class AWS(object):
+
+    def __init__(self, token, secret,
+                 region="us-east-1"):
+        self.region = region
+        self.conn = ec2.connect_to_region(region, aws_access_key_id=token,
+                                          aws_secret_access_key=secret)
 
 
+    def __get_instance(self, instance_id):
+        reservations = self.conn.get_all_instances(instance_ids=[instance_id])
+        machines = list(chain.from_iterable([i.instances for i in reservations]))
+        for m in machines:
+            if m.id == instance_id:
+                return m
+        return m
+
+
+    def sync_keypair(self, key_name, path):
+        """Creates and syncs the keypair"""
+        log.debug("Syncing {0} from {1}".format(key_name, self.region))
+        key_path = os.path.join(path, "{0}.pem".format(key_name))
+        try:
+            key = self.conn.create_key_pair(key_name)
+            key.save(path)
+        except:
+            if not os.path.exists(key_path):
+                log.error("AWS Key {0}.pem already generated but couldn't find it in {1}".format(key_name, path))
+                log.error("This usually means that the keyfile was deleted locally.")
+                log.error("Generate the keypair manually and place {0}.pem in {1}".format(key_name, path))
+                return None
+            else:
+                log.debug("Found {0}.pem. Keys are in sync".format(key_name))
+        return key_path
+
+
+    def sync_security_group(self, security_group):
+        log.debug("Authorizing security group for minecraft")
+        sec_groups = [x for x in self.conn.get_all_security_groups() if x.name == security_group]
+        sec_group = None
+        if not sec_groups:
+            log.debug("Creating security group %s", security_group)
+            sec_group = self.conn.create_security_group(security_group, "Minecraft server security group")
+        else:
+            sec_group = sec_groups[0]
+        ports = [25565, 22] # minecraft and ssh
+        existing_rules = [int(x.from_port) for x in sec_group.rules]
+        for p in ports:
+            if p in existing_rules:
+                log.debug("Port %s already open", p)
+            else:
+                log.debug("Opening up port %s", p)
+                sec_group.authorize('tcp', p, p, '0.0.0.0/0')
+        
+
+    def spawn(self, name, ami=None, instance_type="t1.micro",
+              key_name="cloudcraft", security_group="cloudcraft",
+              user="root"):
+        image = self.conn.get_image(ami)
+#        res = image.run(1, 1, instance_type=instance_type,
+#                        key_name=key_name, security_groups=["default", security_group])
+#        machine = res.instances[0]
+        log.info("Spawning '{0}'. This will take a couple of minutes...".format(name))
+#        while machine.update() == "pending":
+#            time.sleep(2)
+#        log.debug("Instance state changed from 'pending' to {0}".format(machine.state))
+#        if machine.state != 'running':
+#            log.error("Couldn't start instance {0}. Please destroy the stale instance by logging in to your AWS console. This will be fixed in the future".format(machine.id))
+#            return None
+        
+        #mcserver = mcserver.MinecraftServer(machine.id, name)
+        machine = self.__get_instance("i-dec75deb")
+        mcs = mcserver.MinecraftServer(machine.id, name,
+                                       user, machine.public_dns_name)
+        return mcs
+
+
+    def destroy(self, mcserver):
+        m = self.__get_instance(mcserver.server_id)
+        log.debug("Terminating instance %s", m.id)
+        return m.terminate()
+
+
+    def shutdown(self, mcserver):
+        m = self.__get_instance(mcserver.server_id)
+        log.debug("Stopping instance %s", m.id)
+        return m.stop()
+
+
+    def boot(self, mcserver):
+        m = self.__get_instance(mcserver.server_id)
+        log.debug("Booting up instance %s", m.id)
+        return m.start()
+
+
+    def reboot(self, mcserver):
+        m = self.__get_instance(mcserver.server_id)
+        log.debug("Rebooting instance %s", m.id)
+        return m.reboot()
+
+     
+        
+                
+    
+        
+"""
 def __save_keypair(conn, keyname, path):
     # FIXME: This is rather naive. Handle this a bit smartly
     key_root = os.path.join(path, "keys")
@@ -41,7 +143,7 @@ def __delete_keypair(conn, keyname):
     conn.delete_key_pair(keyname)
     # Delete from disk as well
 
-
+    
 def provision(cloudcraft_home, name, aws_access_token="", aws_access_secret="",
               ec2_region="us-west-2", ami="ami-bf1d8a8f",
               instance_type="m1.small", keyname=None, security_group=None):
@@ -53,7 +155,7 @@ def provision(cloudcraft_home, name, aws_access_token="", aws_access_secret="",
     if not keyname:
         keyname = "cloudcraft-{0}".format(ec2_region)
         log.debug("Creating Keypair - {0}".format(keyname))
-        keypair = __save_keypair(conn, keyname, cloudcraft_home)
+        keypair = __save_keypair(conn, keyname, cloudcraft_home
         if not keypair:
             return {}
 
@@ -100,11 +202,7 @@ def destroy(instance, aws_access_token="", aws_access_secret="",
                             ec2_region)
     machine_id = instance["id"]
     machines = list(chain.from_iterable([i.instances for i in conn.get_all_instances(instance_ids=[machine_id])]))
-    for m in machines:
-        if m.id == machine_id and m.state != "terminated":
-            log.info("Terminating %s" % instance["mcs_name"])
-            m.terminate()
-            return True
     log.error("%s not found or already terminated. Are you sure if it exists?" % (machine_id))
     return False
 
+"""
