e4afa97d7984607b2dcb797585aa516a59e35da0
Sudharshan S <sudharsh@gmail.com>
2013-06-02 01:02:05 +0530

    Don't be lazy and use os.path.join

--- cloudcraft/commander.py
+++ cloudcraft/commander.py
@@ -21,8 +21,8 @@ log = logging.getLogger("cloudcraft")
 def run_remote(command, command_args=[], remote_vars={}):
     try:
         run("mkdir -p %s" % REMOTE_PATH)
-        script_file = "{0}/{1}.sh".format(SCRIPTS_PATH, command)
-        remote_path = "{0}/{1}.sh".format(REMOTE_PATH, command)
+        script_file = os.path.join(SCRIPTS_PATH, "{0}.sh".format(command))
+        remote_path = os.path.join(REMOTE_PATH, "{0}.sh".format(command))
         prepared_buffer = StringIO.StringIO()
         for k, v in remote_vars.items():
             prepared_buffer.write('%s="%s"\n' %(k,v))
@@ -30,7 +30,7 @@ def run_remote(command, command_args=[], remote_vars={}):
             prepared_buffer.write(fh.read())
         if command == "bootstrap":
             print(green("----- Syncing lib.sh -----"))
-            put(SCRIPTS_PATH + "lib.sh", REMOTE_PATH + "lib.sh")
+            put(os.path.join(SCRIPTS_PATH, "lib.sh"), os.path.join(REMOTE_PATH, "lib.sh"))
         put(prepared_buffer, remote_path)
         with cd(REMOTE_PATH):
             return run("bash ./{0}.sh {1}".format(command, " ".join(command_args)), pty=False, combine_stderr=False)
