c975331eed5497814f7d45871fd7f84b20e25482
Sudharshan S <sudharsh@gmail.com>
2013-06-01 22:20:36 +0530

    New classifiers

--- setup.py
+++ setup.py
@@ -2,16 +2,29 @@
 
 from distutils.core import setup
 
-setup(name="Cloudcraft",
+setup(name="cloudcraft",
       license="BSD",
       version="0.1",
       description="Easy minecraft and bukkit server management",
+      long_description="Cloudcraft is a toolbelt to manage bukkit servers easily on the cloud",
       author="Sudharshan S",
       author_email="sudharsh@gmail.com",
       url="https://github.com/sudharsh/cloudcraft",
       packages=['cloudcraft'],
       package_data={'cloudcraft': ['scripts/*.sh', 'config/*.template']},
-      scripts=['bin/cloudcraft']
+      scripts=['bin/cloudcraft'],
+
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Utilities'
+        ],
+      
+      requires=["Fabric (==1.6.1)",
+                "boto (==2.9.4)",
+                "paramiko (==1.10.1)"]
       )
       
 
