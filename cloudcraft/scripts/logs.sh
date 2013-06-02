045b061403612054b585502c77c110e7c1c03e04
Sudharshan S <sudharsh@gmail.com>
2013-06-02 23:34:48 +0530

    Add tail arg to log

--- cloudcraft/scripts/logs.sh
+++ cloudcraft/scripts/logs.sh
@@ -1,1 +1,8 @@
-tail -n 50 ${mc_home}/server.log
+command=${1}
+echo $command
+if [ $command == "tail" ]
+then
+    tail -n 50 -f ${mc_home}/server.log
+else
+    tail -n 50 ${mc_home}/server.log
+fi
